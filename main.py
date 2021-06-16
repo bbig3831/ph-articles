803291a8d2a7ac9c018e6bb69581ab0ee21741d5
bbig3831 <beb601@mail.harvard.edu>
2021-06-15 21:44:47 -0400

    Use loop for scraping pages by each month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
-from urllib.parse import urljoin
+from datetime import datetime
+from itertools import product
 
 from bs4 import BeautifulSoup
 from newspaper import Article
@@ -7,30 +8,37 @@ import pandas as pd
 import requests
 
 
-KHN_URL = 'https://khn.org/news/'
-YEARS = range(2008, 2022)
+KHN_URL = 'https://khn.org/news'
+YEARS = range(2009, 2011)
 MONTHS = range(1, 13)
 
 KhnArticleTuple = namedtuple('KhnArticle', 'headline url publish_dat')
 
 
-def build_article_list(month: int, year: int):
+def build_article_list(year: int, month: int) -> list:
     # Get content
-    url = urljoin(KHN_URL, str(year), str(month))
-    r = requests.get(url)
-    doc = BeautifulSoup(r.content, 'html.parser')
-    results_section = doc.find('section', attrs={'class': 'article-list-results'})
-    headlines = results_section.find_all('p', attrs={'class': 'headline'})
-    # Parse content
+    page = 1
     headlines_list = []
-    for headline in headlines:
-        data_dict = {
-            'headline': headline.a.text,
-            'url': headline.a.get('href')
-        }
-        headlines_list.append(data_dict)
+    while True:
+        url = f'{KHN_URL}/{str(year)}/{str(month)}/page/{page}'
+        print(f'Getting {url}')
+        r = requests.get(url)
+        if r.status_code != requests.codes.ok:
+            print(f'{url} not found')
+            break
+        doc = BeautifulSoup(r.content, 'html.parser')
+        results_section = doc.find('section', attrs={'class': 'article-list-results'})
+        headlines = results_section.find_all('p', attrs={'class': 'headline'})
+        # Parse content
+        for headline in headlines:
+            data_dict = {
+                'headline': headline.a.text,
+                'url': headline.a.get('href')
+            }
+            headlines_list.append(data_dict)
+        page += 1
 
-    print(headlines_list)
+    return headlines_list
 
 
 def download_articles():
@@ -42,4 +50,13 @@ def main():
 
 
 if __name__ == '__main__':
-    build_article_list(month=3, year=2020)
+
+    article_list = []
+    now = datetime.now()
+    for year, month in product(YEARS, MONTHS):
+        if (year < now.year) or (year == now.year and month <= now.month):
+            print(f'Building article list for {year}-{month}')
+            article_list += build_article_list(year=year, month=month)
+
+    for article in article_list[0:10]:
+        print(f"{article['headline']} - {article['url']}")
